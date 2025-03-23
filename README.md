51056ad15cdd270f4d7d7eaa04df96f4313f297c
Kalle Palomäki <kpalomak@Kalles-MacBook-Pro.local>
2025-03-23 14:23:36 +0200

    added run_sql_agent.py and edited .gitignore for PyCharm

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Nortwind-Database-Agent
+# Langchain experiments
+
+<img width="1311" alt="kuva" src="https://github.com/user-attachments/assets/340dd057-3c36-4a9d-98fe-737411e6e8fc" />
+
+S-market bot fills automatically a shopping cart by a natural language query.
+
+This project is to make database queries using natural language in a controlled manner by AI agents using python language chain
+and OpenAI api. I have used publicly available Northwind database to demonstrate the approach. 
+
+Rather letting AI to generate SQL freely I have applied two stage approach. First, I provide query templates for anticipated complex 
+queries for which agent finds only calling parameters. Second, if template based approaches do not answer users
+question, then free query is allowed. The priority of using the template is achieved by instructing the agent to 
+try template based functions before calling free SQL as follows. Here is the corresponding instruction to the agent.
+"Use this as a fallback tool for free-form SQL queries after you have tried other agents multiple times."
+
+# Dependencies
+
+I'm using python 3.10. for the experiments
+
+For Northwind I'm using SQLite3 tools 
+from this repo:
+https://github.com/jpwhite3/northwind-SQLite3
+
+Follow the instructions therein to install Northwind db. For this code I have done the all the three steps in the build instructions
+make build 
+make populate
+make report
---
+++ run_sql_agent.py
@@ -0,0 +1,160 @@
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+from langchain_openai import ChatOpenAI
+import sqlite3
+import os
+import json
+from langchain.tools import Tool
+
+# Connect to (or create) a database file
+
+conn = sqlite3.connect("../northwind-SQLite3/dist/northwind.db")
+
+with open("../openai.key") as f:
+    os.environ["OPENAI_API_KEY"] = f.read().strip().replace("OPENAI_API_KEY=", "")
+
+# Function to get schema information dynamically from the database
+def get_schema_info():
+
+    # Get all table names
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+
+    schema_info = "Database Schema:\n"
+
+    for table in tables:
+        table_name = '"' + table[0] + '"'
+        schema_info += f"\nTable: {table_name}\nColumns:\n"
+
+        # Get columns for each table
+        cursor.execute(f"PRAGMA table_info({table_name});")
+        columns = cursor.fetchall()
+
+        for column in columns:
+            column_name = column[1]
+            schema_info += f"  - {column_name}\n"
+
+    return schema_info
+
+# Function to execute free SQL query and return results
+def execute_sql_query(sql_query):
+
+    try:
+        cursor.execute(sql_query)  # Execute the generated SQL query
+        result = cursor.fetchall()
+        return result
+    except Exception as e:
+        return f"Error executing query: {str(e)}"
+
+def n_largest_customers_by_revenue(N):
+    try:
+        cursor.execute("""
+        SELECT 
+            c.CompanyName AS Customer, 
+            SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)) AS TotalRevenue
+        FROM "Order Details" od
+        JOIN Orders o ON od.OrderID = o.OrderID
+        JOIN Customers c ON o.CustomerID = c.CustomerID
+        GROUP BY c.CustomerID
+        ORDER BY TotalRevenue DESC
+        LIMIT """ + str(N) + ";" )
+
+        rows = cursor.fetchall()
+        return rows
+    except:
+        print("failed n_largest_customers_by_revenue")
+
+def revenue_range(rang: str):
+    try:
+        print(rang)
+        data=json.loads(rang)
+        min_revenue=data["mini"]
+        max_revenue=data["maxi"]
+
+        query = """
+            SELECT 
+                c.CompanyName AS Customer, 
+                SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)) AS TotalRevenue
+            FROM "Order Details" od
+            JOIN Orders o ON od.OrderID = o.OrderID
+            JOIN Customers c ON o.CustomerID = c.CustomerID
+            GROUP BY c.CustomerID
+            HAVING TotalRevenue BETWEEN """ + str(min_revenue) + " AND " + str(max_revenue) + """ 
+            ORDER BY TotalRevenue DESC;
+        """
+        print(query)
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        return rows
+    except:
+        print("failed")
+
+# Create a cursor to execute SQL commands
+cursor = conn.cursor()
+schema_info = get_schema_info()
+
+tools = [
+    Tool(
+        name="n_largest_customers_by_revenue",
+        func=n_largest_customers_by_revenue,
+        description=(
+            "Use this to determine the largest companies by revenue. "
+            "Provide this number as a string with a single integer. "
+            "Do not include anything else in the response."
+        ),
+    ),
+    Tool(
+        name="revenue_range",
+        func=revenue_range,
+        description=(
+            "Use this to determine companies within a certain range of revenue. "
+            "Provide the range as JSON-formatted string with keys mini and maxi"
+            "and values as integers use the JSON to call function that includes pre-defined SQL template"
+            "that searches the database of interest"
+            "Do not include anything else in the response."
+        ),
+    ),
+
+    Tool(
+        name="sql_tool",
+        func=execute_sql_query,  # Directly use the SQL execution function
+        description=f"""
+        Use this as a fallback tool for free-form SQL queries after you have tried other agents multiple times.
+        Use this tool to generate and execute SQL queries on the Northwind database based on natural language input. 
+        The schema info is already embedded within the tool description:
+
+        {schema_info}
+
+        Your task is to generate SQL queries based on the natural language input, using the database schema information provided above.
+        """
+    ),
+
+]
+
+# Get column names
+
+llm = ChatOpenAI(
+    model="gpt-4",
+    temperature=0.5,
+    max_tokens=2000,
+    timeout=30,
+    max_retries=2,
+    api_key=os.getenv("OPENAI_API_KEY")  # Using the environment variable
+)
+
+# Initialize the agent
+agent = initialize_agent(
+    tools=tools,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    llm=llm,
+    verbose=True
+)
+
+# Run the agent with a prompt to find quotes
+#prompt = "Please find companies with revenue between 4 M and 6 M in brackets of 200 k. List each bracket separately."# Käytä yhden tain kahden sanan ilmaisua per tuote. Kokoa tuotteet python listaan. Älä sisällytä vastaukseen mitään muuta kuin lista. Voitko etsiä listan tuotteet minulle koko lista kerrallaan?"
+#prompt= "Please find top 2 companies"
+#prompt="Find the last name name of all the employees and their birth dates."
+prompt=input("query: ")
+result = agent.invoke(prompt)
+print(result["input"])
+print(result["output"])
